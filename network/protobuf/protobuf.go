3be26f0b261506c4f315811cc756fc38910161e7
yanjq <yanjq@realbull.net>
2018-04-10 19:45:22 +0800

    修改protobuffer解码

--- network/protobuf/protobuf.go
+++ network/protobuf/protobuf.go
@@ -17,10 +17,6 @@ import (
 // -------------------------
 type Processor struct {
 	littleEndian bool
-	//msgInfo      []*MsgInfo
-	//msgID        map[reflect.Type]uint16
-	//marshalHandler func(msg interface{}) ([][]byte, error)
-	//unmarshalHandler func(data []byte) (interface{}, error)
 	msgInfo map[string]*MsgInfo
 
 }
@@ -42,7 +38,6 @@ type MsgRaw struct {
 func NewProcessor() *Processor {
 	p := new(Processor)
 	p.littleEndian = false
-	//p.msgID = make(map[reflect.Type]uint16)
 	p.msgInfo = make(map[string]*MsgInfo)
 	return p
 }
@@ -58,20 +53,6 @@ func (p *Processor) Register(msg proto.Message) string {
 	if msgType == nil || msgType.Kind() != reflect.Ptr {
 		log.Fatal("protobuf message pointer required")
 	}
-	//fmt.Println(msgType.Elem().Name())
-	//if _, ok := p.msgID[msgType]; ok {
-	//	log.Fatal("message %s is already registered", msgType)
-	//}
-	//if len(p.msgInfo) >= math.MaxUint16 {
-	//	log.Fatal("too many protobuf messages (max = %v)", math.MaxUint16)
-	//}
-	//
-	//i := new(MsgInfo)
-	//i.msgType = msgType
-	//p.msgInfo = append(p.msgInfo, i)
-	//id := uint16(len(p.msgInfo) - 1)
-	//p.msgID[msgType] = id
-	//return id
 	msgID := msgType.Elem().Name()
 	if msgID == "" {
 		log.Fatal("unnamed json message")
@@ -83,23 +64,11 @@ func (p *Processor) Register(msg proto.Message) string {
 	i := new(MsgInfo)
 	i.msgType = msgType
 	p.msgInfo[msgID] = i
-	fmt.Println(8888, p.msgInfo)
 	return msgID
 }
 
 // It's dangerous to call the method on routing or marshaling (unmarshaling)
 func (p *Processor) SetRouter(msg proto.Message, msgRouter *chanrpc.Server) {
-	//msgType := reflect.TypeOf(msg)
-	//if msgType == nil || msgType.Kind() != reflect.Ptr {
-	//	log.Fatal("json message pointer required")
-	//}
-	//msgID := msgType.Elem().Name()
-	//i, ok := p.msgInfo[msgID]
-	//if !ok {
-	//	log.Fatal("message %v not registered", msgID)
-	//}
-	//
-	//i.msgRouter = msgRouter
 	msgType := reflect.TypeOf(msg)
 	if msgType == nil || msgType.Kind() != reflect.Ptr {
 		log.Fatal("json message pointer required")
@@ -116,17 +85,6 @@ func (p *Processor) SetRouter(msg proto.Message, msgRouter *chanrpc.Server) {
 
 // It's dangerous to call the method on routing or marshaling (unmarshaling)
 func (p *Processor) SetHandler(msg proto.Message, msgHandler MsgHandler) {
-	//msgType := reflect.TypeOf(msg)
-	//if msgType == nil || msgType.Kind() != reflect.Ptr {
-	//	log.Fatal("json message pointer required")
-	//}
-	//msgID := msgType.Elem().Name()
-	//i, ok := p.msgInfo[msgID]
-	//if !ok {
-	//	log.Fatal("message %v not registered", msgID)
-	//}
-	//
-	//i.msgHandler = msgHandler
 	msgType := reflect.TypeOf(msg)
 	if msgType == nil || msgType.Kind() != reflect.Ptr {
 		log.Fatal("json message pointer required")
@@ -185,34 +143,10 @@ func (p *Processor) Route(msg interface{}, userData interface{}) error {
 
 // goroutine safe
 func (p *Processor) Unmarshal(data []byte) (interface{}, error) {
-	//if len(data) < 2 {
-	//	return nil, errors.New("protobuf data too short")
-	//}
-	//
-	//// id
-	//var id uint16
-	//if p.littleEndian {
-	//	id = binary.LittleEndian.Uint16(data)
-	//} else {
-	//	id = binary.BigEndian.Uint16(data)
-	//}
-	//if id >= uint16(len(p.msgInfo)) {
-	//	return nil, fmt.Errorf("message id %v not registered", id)
-	//}
-	//
-	//// msg
-	//i := p.msgInfo[id]
-	//if i.msgRawHandler != nil {
-	//	return MsgRaw{id, data[2:]}, nil
-	//} else {
-	//	msg := reflect.New(i.msgType.Elem()).Interface()
-	//	return msg, proto.UnmarshalMerge(data[2:], msg.(proto.Message))
-	//}
 
 	var m map[string][]byte
 	err := json.Unmarshal(data, &m)
 	if err != nil {
-		fmt.Println(1111)
 		return nil, err
 	}
 	if len(m) != 1 {
@@ -240,26 +174,6 @@ func (p *Processor) Unmarshal(data []byte) (interface{}, error) {
 
 // goroutine safe
 func (p *Processor) Marshal(msg interface{}) ([][]byte, error) {
-	//msgType := reflect.TypeOf(msg)
-	//
-	//// id
-	//_id, ok := p.msgID[msgType]
-	//if !ok {
-	//	err := fmt.Errorf("message %s not registered", msgType)
-	//	return nil, err
-	//}
-	//
-	//id := make([]byte, 2)
-	//if p.littleEndian {
-	//	binary.LittleEndian.PutUint16(id, _id)
-	//} else {
-	//	binary.BigEndian.PutUint16(id, _id)
-	//}
-	//
-	//// data
-	//data, err := proto.Marshal(msg.(proto.Message))
-	//return [][]byte{id, data}, err
-
 	msgType := reflect.TypeOf(msg)
 	if msgType == nil || msgType.Kind() != reflect.Ptr {
 		return nil, errors.New("json message pointer required")
@@ -279,24 +193,4 @@ func (p *Processor) Marshal(msg interface{}) ([][]byte, error) {
 	return [][]byte{data}, err
 }
 
-// goroutine safe
-//func (p *Processor) Range(f func(id uint16, t reflect.Type)) {
-//	for id, i := range p.msgInfo {
-//		f(uint16(id), i.msgType)
-//	}
-//
-//	//
-//	msgType := reflect.TypeOf(msg)
-//	if msgType == nil || msgType.Kind() != reflect.Ptr {
-//		return nil, errors.New("json message pointer required")
-//	}
-//	msgID := msgType.Elem().Name()
-//	if _, ok := p.msgInfo[msgID]; !ok {
-//		return nil, fmt.Errorf("message %v not registered", msgID)
-//	}
-//
-//	// data
-//	m := map[string]interface{}{msgID: msg}
-//	data, err := json.Marshal(m)
-//	return [][]byte{data}, err
-//}
+
